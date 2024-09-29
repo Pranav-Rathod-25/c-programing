a69362ba48de96b77aac756b2529527e330e4469
Pranav-Rathod-25 <pranav.vrathod25@gmail.com>
2024-09-29 14:57:24 +0530

    addition.c

---
+++ 01-addition/addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int numberOne = 1;
+int numberTwo = 2;
+int result =0;
+
+int main(void)
+{
+	result = numberOne + numberTwo;
+	printf("\n %d + %d =  %d\n",numberOne, numberTwo , result);
+	return(0);
+}
+
